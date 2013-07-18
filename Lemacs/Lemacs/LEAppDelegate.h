7a33055d6878c6277066433e3166712a4d4a0626
Mike Lee <bmf.home@me.com>
2013-07-18 15:58:41 +0200

    Lemacs is a conversation-based tool that combines the social collaboration of Twitter with the workflow and backend of GitHub.

---
+++ Lemacs/Lemacs/LEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LEAppDelegate.h
+//  Lemacs
+//
+//  Created by Mike Lee on 7/18/13.
+//  Copyright (c) 2013 New Lemurs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lemacs/LemacsTests/LemacsTests.m
@@ -0,0 +1,36 @@
+//
+//  LemacsTests.m
+//  LemacsTests
+//
+//  Created by Mike Lee on 7/18/13.
+//  Copyright (c) 2013 New Lemurs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LemacsTests : XCTestCase
+
+@end
+
+@implementation LemacsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
