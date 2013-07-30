4daf6641b1c49e20f2067a673af9046a77d69b93
Mike Lee <bmf.home@me.com>
2013-07-30 11:11:23 +0200

    Update this todo, and add an issue number, which should be the new standard.

--- Lemacs/LETalkListController.h
+++ Lemacs/LETalkListController.h
@@ -5,7 +5,7 @@
 //  Created by Mike Lee on 7/18/13.
 //  Copyright (c) 2013 New Lemurs. All rights reserved.
 //
-// TODO: Factor out common LCTalkController
+// TODO: Make LCTalkListController a subclass of LETalkViewController, refs #26
 
 @class LETalkViewController, UAGithubEngine;
 
@@ -15,6 +15,7 @@
 
 - (IBAction)reloadList;
 - (IBAction)resizeCells:(UIPinchGestureRecognizer *)pinch;
+- (IBAction)showOptions:(UIBarButtonItem *)barButton;
 
 @end
 
